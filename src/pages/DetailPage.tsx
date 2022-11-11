1496265ba45b116ee152012093cb63627addb2d1
Steve NGUYEN <snguyen@fff.fr>
2022-11-11 17:05:17 +0100

    fix centering responsive

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,8 +1,16 @@
-import { useStore } from "../lib/store"
-import { CardContent, Card, CardMedia, Typography, Grid, Box, Button } from '@mui/material'
-import { useEffect, useState } from "react"
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import { useNavigate } from "react-router-dom";
+import { useStore } from '../lib/store'
+import {
+  CardContent,
+  Card,
+  CardMedia,
+  Typography,
+  Grid,
+  Box,
+  Button,
+} from '@mui/material'
+import { useEffect, useState } from 'react'
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
+import { useNavigate } from 'react-router-dom'
 export const DetailPage = () => {
   const { player } = useStore()
   const [cname, setCname] = useState<string>('')
@@ -10,15 +18,17 @@ export const DetailPage = () => {
 
   useEffect(() => {
     if (player)
-      fetch("https://restcountries.com/v3.1/alpha/" + player.country.code)
-        .then(response => response.json())
-        .then(cdatas => cdatas.map((cdata: any) => setCname(cdata.name.common)))
+      fetch('https://restcountries.com/v3.1/alpha/' + player.country.code)
+        .then((response) => response.json())
+        .then((cdatas) =>
+          cdatas.map((cdata: any) => setCname(cdata.name.common))
+        )
   }, [])
 
   const handleImageError = (e: any) => {
-    e.target.onerror = null;
-    // e.target.style.display = 'none'
-    e.target.src = "https://upload.wikimedia.org/wikipedia/commons/2/20/Flag_of_the_United_States_%281912%E2%80%931959%29.jpg"
+    e.target.onerror = null
+    e.target.src =
+      'https://upload.wikimedia.org/wikipedia/commons/2/20/Flag_of_the_United_States_%281912%E2%80%931959%29.jpg'
   }
 
   const goToHomePage = () => {
@@ -26,17 +36,34 @@ export const DetailPage = () => {
   }
 
   if (player) {
-    const { data, } = player
+    const { data } = player
     const { points, rank, age, height, weight } = data
     return (
-      <>
-        <Button sx={{ position: 'absolute', top: '2rem', right: '50%' }} onClick={goToHomePage} >
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          minWidth: '75rem',
+        }}
+      >
+        <Button
+          sx={{ display: 'block', zIndex: 10, marginTop: '1rem' }}
+          onClick={goToHomePage}
+        >
           <CloseRoundedIcon sx={{ color: 'black', objectFit: 'cover' }} />
         </Button>
         <Box sx={{ display: 'block', margin: 'auto' }}>
-          <Card sx={{ minWidth: '75rem', maxHeight: '43rem', display: 'flex', margin: '5rem 3rem 0 3rem' }}>
+          <Card
+            sx={{
+              minWidth: '75rem',
+              minHeight: '35rem',
+              display: 'flex',
+              margin: '1rem 30px 0 30px',
+            }}
+          >
             <CardMedia
-              component="img"
+              component='img'
               image={player.picture}
               sx={{
                 display: 'block',
@@ -46,127 +73,149 @@ export const DetailPage = () => {
               }}
             />
             <CardContent sx={{ marginRight: '-2rem' }}>
-              <Typography component="div" variant="h1" sx={{
-                fontFamily: "Montserrat Bold",
-                color: "var(--orange)",
-                marginLeft: "-20%"
-              }}>
+              <Typography
+                component='div'
+                variant='h1'
+                sx={{
+                  fontFamily: 'Montserrat Bold',
+                  color: 'var(--orange)',
+                  marginLeft: '-20%',
+                }}
+              >
                 {player.firstname} {player.lastname}
               </Typography>
-              <Grid container columnSpacing={12} sx={{ marginTop: 10, marginRight: 10 }} rowSpacing={6}>
+              <Grid
+                container
+                columnSpacing={12}
+                sx={{ marginTop: 10, marginRight: 10 }}
+                rowSpacing={6}
+              >
                 <Grid item xs={4}>
                   <Typography
                     sx={{
                       fontFamily: 'Mulish Bold',
                       color: 'var(--black)',
-                      opacity: '30%'
-                    }}>
+                      opacity: '30%',
+                    }}
+                  >
                     RANK
                   </Typography>
                   <Typography
                     sx={{
                       fontFamily: 'Montserrat Bold',
-                      color: 'var(--orange)'
+                      color: 'var(--orange)',
                     }}
                   >
                     #{rank}
                   </Typography>
                 </Grid>
                 <Grid item xs={4}>
-                  <Typography sx={{
-                    fontFamily: 'Mulish Bold',
-                    color: 'var(--black)',
-                    opacity: '30%'
-                  }}>
+                  <Typography
+                    sx={{
+                      fontFamily: 'Mulish Bold',
+                      color: 'var(--black)',
+                      opacity: '30%',
+                    }}
+                  >
                     POINTS
                   </Typography>
                   <Typography
                     sx={{
                       fontFamily: 'Montserrat Bold',
-                      color: 'var(--orange)'
-                    }}>
+                      color: 'var(--orange)',
+                    }}
+                  >
                     {points}
                   </Typography>
                 </Grid>
-                <Grid item xs={4} >
-                  <Typography sx={{
-                    fontFamily: 'Mulish Bold',
-                    color: 'var(--black)',
-                    opacity: '30%'
-                  }}>
+                <Grid item xs={4}>
+                  <Typography
+                    sx={{
+                      fontFamily: 'Mulish Bold',
+                      color: 'var(--black)',
+                      opacity: '30%',
+                    }}
+                  >
                     COUNTRY
                   </Typography>
                   <Typography
                     sx={{
                       fontFamily: 'Montserrat Bold',
-                      color: 'var(--orange)'
+                      color: 'var(--orange)',
                     }}
                   >
                     {cname}
                   </Typography>
                 </Grid>
                 <Grid item xs={4}>
-                  <Typography sx={{
-                    fontFamily: 'Mulish Bold',
-                    color: 'var(--black)',
-                    opacity: '30%'
-                  }}>
+                  <Typography
+                    sx={{
+                      fontFamily: 'Mulish Bold',
+                      color: 'var(--black)',
+                      opacity: '30%',
+                    }}
+                  >
                     BIRTHDAY
                   </Typography>
                   <Typography
                     sx={{
                       fontFamily: 'Montserrat Bold',
-                      color: 'var(--orange)'
-                    }}>
-
-                  </Typography>
+                      color: 'var(--orange)',
+                    }}
+                  ></Typography>
                 </Grid>
                 <Grid item xs={8}>
-                  <Typography sx={{
-                    fontFamily: 'Mulish Bold',
-                    color: 'var(--black)',
-                    opacity: '30%'
-                  }}>
+                  <Typography
+                    sx={{
+                      fontFamily: 'Mulish Bold',
+                      color: 'var(--black)',
+                      opacity: '30%',
+                    }}
+                  >
                     AGE
                   </Typography>
                   <Typography
                     sx={{
                       fontFamily: 'Montserrat Bold',
-                      color: 'var(--orange)'
+                      color: 'var(--orange)',
                     }}
                   >
                     {age}
                   </Typography>
                 </Grid>
                 <Grid item xs={4}>
-                  <Typography sx={{
-                    fontFamily: 'Mulish Bold',
-                    color: 'var(--black)',
-                    opacity: '30%'
-                  }}>
+                  <Typography
+                    sx={{
+                      fontFamily: 'Mulish Bold',
+                      color: 'var(--black)',
+                      opacity: '30%',
+                    }}
+                  >
                     WEIGHT
                   </Typography>
                   <Typography
                     sx={{
                       fontFamily: 'Montserrat Bold',
-                      color: 'var(--orange)'
+                      color: 'var(--orange)',
                     }}
                   >
                     {weight / 1000} kg
                   </Typography>
                 </Grid>
                 <Grid item xs={4}>
-                  <Typography sx={{
-                    fontFamily: 'Mulish Bold',
-                    color: 'var(--black)',
-                    opacity: '30%'
-                  }}>
+                  <Typography
+                    sx={{
+                      fontFamily: 'Mulish Bold',
+                      color: 'var(--black)',
+                      opacity: '30%',
+                    }}
+                  >
                     HEIGHT
                   </Typography>
                   <Typography
                     sx={{
                       fontFamily: 'Montserrat Bold',
-                      color: 'var(--orange)'
+                      color: 'var(--orange)',
                     }}
                   >
                     {height} cm
@@ -174,10 +223,10 @@ export const DetailPage = () => {
                 </Grid>
               </Grid>
             </CardContent>
-            <Grid container direction="column" alignItems="center" spacing={4}>
+            <Grid container direction='column' alignItems='center' spacing={4}>
               <Grid item>
                 <CardMedia
-                  component="img"
+                  component='img'
                   image={player.country.picture}
                   sx={{
                     display: 'flex',
@@ -191,18 +240,15 @@ export const DetailPage = () => {
                 />
               </Grid>
               <Grid item>
-                <Typography variant="h2" sx={{ color: 'grey' }}>
+                <Typography variant='h2' sx={{ color: 'grey' }}>
                   {player.country.code}
                 </Typography>
               </Grid>
             </Grid>
           </Card>
         </Box>
-      </>
+      </Box>
     )
-  }
-  else return (
-    <p> You have refreshed the page! Please go back to homepage. </p>
-  )
-
+  } else
+    return <p> You have refreshed the page! Please go back to homepage. </p>
 }
