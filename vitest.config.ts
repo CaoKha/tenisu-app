da8ddfcbf722855b836a5cee98bab0e912bec15f
Steve NGUYEN <snguyen@fff.fr>
2022-11-17 13:17:49 +0100

    testing coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,12 @@ import { configDefaults, defineConfig } from "vitest/config"
 
 export default defineConfig({
   test: {
-    exclude: [...configDefaults.exclude, 'tests/e2e/*']
+    exclude: [...configDefaults.exclude, 'tests/e2e/*'],
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html'],
+      all: true,
+      include: ["src/components/search-bar.tsx"]
+    }
   }
 })
